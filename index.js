fe95ab2601e8c27065ffca9f0fdfd0727fb0894e
Vishal Kamra <101691491+vishal-tiket@users.noreply.github.com>
2023-07-13 02:12:46 +0530

    show user agent

--- index.js
+++ index.js
@@ -1,60 +1,57 @@
 // Function to detect the browser using the user agent
 function detectBrowser() {
-  if (typeof window !== "undefined") {
-    var userAgent = navigator.userAgent;
+  var userAgent = navigator.userAgent;
 
-    /**
-     * this commented code is the final logic which should be used
-     */
-    // const userAgentsArray = [
-    //   "Firefox",
-    //   "Chrome",
-    //   "Safari",
-    //   "Opera",
-    //   "OPR",
-    //   "Edge",
-    //   "Trident",
-    //   "Edg",
-    // ];
+  /**
+   * this commented code is the final logic which should be used
+   */
+  // const userAgentsArray = [
+  //   "Firefox",
+  //   "Chrome",
+  //   "Safari",
+  //   "Opera",
+  //   "OPR",
+  //   "Edge",
+  //   "Trident",
+  //   "Edg",
+  // ];
 
-    //exceptions
-    // if(userAgent.indexOf('Slack') > -1){
-    //    return false
-    //}
+  //exceptions
+  // if(userAgent.indexOf('Slack') > -1){
+  //    return false
+  //}
 
-    // const isBrowser = userAgentsArray.some((agent) => {
-    //   return userAgent.indexOf(agent) > -1;
-    // });
+  // const isBrowser = userAgentsArray.some((agent) => {
+  //   return userAgent.indexOf(agent) > -1;
+  // });
 
-    // console.log(isBrowser)
+  // console.log(isBrowser)
 
-    /**
-     * showing browser name just for POC
-     */
-    if (userAgent.indexOf("Slack") > -1) {
-      return "In-App Browser";
-    }
-    if (userAgent.indexOf("Firefox") > -1) {
-      return "Mozilla Firefox";
-    } else if (userAgent.indexOf("Chrome") > -1) {
-      return "Google Chrome";
-    } else if (userAgent.indexOf("Safari") > -1) {
-      return "Apple Safari";
-    } else if (
-      userAgent.indexOf("Opera") > -1 ||
-      userAgent.indexOf("OPR") > -1
-    ) {
-      return "Opera";
-    } else if (userAgent.indexOf("Edge") > -1) {
-      return "Microsoft Edge";
-    } else if (userAgent.indexOf("Trident") > -1) {
-      return "Internet Explorer";
-    } else {
-      return "In-App Browser";
-    }
+  /**
+   * showing browser name just for POC
+   */
+  if (userAgent.indexOf("Slack") > -1) {
+    return "In-App Browser";
+  }
+  if (userAgent.indexOf("Firefox") > -1) {
+    return "Mozilla Firefox";
+  } else if (userAgent.indexOf("Chrome") > -1) {
+    return "Google Chrome";
+  } else if (userAgent.indexOf("Safari") > -1) {
+    return "Apple Safari";
+  } else if (userAgent.indexOf("Opera") > -1 || userAgent.indexOf("OPR") > -1) {
+    return "Opera";
+  } else if (userAgent.indexOf("Edge") > -1) {
+    return "Microsoft Edge";
+  } else if (userAgent.indexOf("Trident") > -1) {
+    return "Internet Explorer";
+  } else {
+    return "In-App Browser";
   }
 }
 
-// Usage example
-var browser = detectBrowser();
-document.querySelector("#text").innerHTML = `Web opened in ${browser}`;
+if (typeof window !== "undefined") {
+  var browser = detectBrowser();
+  document.querySelector("#text").innerHTML = `Web opened in ${browser}`;
+  document.querySelector("#user-agent").innerHTML = `${navigator.userAgent}`;
+}
